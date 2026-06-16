64c25c6a1c5d3bd4c91fe6f509c0644d0bf77f15
Krishiv Periwal <krishivperiwal@gmail.com>
2026-06-16 16:42:14 +0530

    Set trust proxy in app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
-
 import authRoutes from "./routes/authRoutes.js";
 import applicationRoutes from "./routes/applicationRoutes.js";
 import interviewRoutes from "./routes/interviewRoutes.js";
@@ -15,7 +14,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.set('trust proxy', 1)
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
